d8a4d11b589a367dab200fec74ef36b2ce3df9ca
= <=>
2021-04-06 12:59:49 +0700

    movDetail,httpSer

---
+++ lib/services/http_service.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+import 'dart:html';
+
+import 'package:http/http.dart' as http;
+
+class HttpService {
+  final String apiKey = '7745248cce48c5838123bc077c473543';
+  final String baseUrl = 'https://api.themoviedb.org/3/movie/popular?api_key=';
+
+  Future<List> getPopularMovies() async {
+    final String uri = baseUrl + apiKey;
+
+    http.Response result = await http.get(Uri.parse(uri));
+    if (result.statusCode == HttpStatus.ok) {
+      print("Sukses");
+      final jsonResponse = json.decode(result.body);
+      final moviesMap = jsonResponse['results'];
+      List movies = moviesMap.map((i) => Movie.fromJson(i)).toList();
+      return movies;
+    } else {
+      print("Fail");
+      return null;
+    }
+  }
+}
